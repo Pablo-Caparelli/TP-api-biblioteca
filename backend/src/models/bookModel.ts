82078351012787c3045e5cd9b4631b5c4ddbce8b
pablo-caparelli <pablocaparelli@gmail.com>
2025-06-26 10:20:17 -0300

    versionKey agregada al Schema

--- backend/src/models/bookModel.ts
+++ backend/src/models/bookModel.ts
@@ -1,12 +1,17 @@
 import { Schema, model } from "mongoose";
 
-const bookSchema = new Schema({
-  titulo: { type: String, required: true, unique: true },
-  autor: { type: String, required: true },
-  año: { type: Number, required: true },
-  genero: { type: String },
-  disponible: { type: Boolean, default: true },
-});
+const bookSchema = new Schema(
+  {
+    titulo: { type: String, required: true, unique: true },
+    autor: { type: String, required: true },
+    año: { type: Number, required: true },
+    genero: { type: String },
+    disponible: { type: Boolean, default: true },
+  },
+  {
+    versionKey: false,
+  }
+);
 
 const Book = model("Book", bookSchema);
 
